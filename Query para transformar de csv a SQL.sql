e486dcaed58d5a3e6e89a6397e58758a9b72f50e
DanSantiagoL <santiagolopez10d@gmail.com>
2025-03-06 18:38:36 -0500

    Se Actualizo la base de datos y el Query

--- Query para transformar de csv a SQL.sql
+++ Query para transformar de csv a SQL.sql
@@ -6,6 +6,7 @@ USE store_inventory;
 -- 2. Crear la tabla store_inventory
 CREATE TABLE store_inventory (
     Transaction_ID INT AUTO_INCREMENT PRIMARY KEY,
+    City VARCHAR(100),
     State VARCHAR(100),
     Country VARCHAR(100),
     Age INT,
@@ -31,6 +32,7 @@ CREATE TABLE store_inventory (
 -- Crear la tabla Location
 CREATE TABLE Location (
     Location_Id INT AUTO_INCREMENT PRIMARY KEY,
+    City VARCHAR(255),
     State VARCHAR(255),
     Country VARCHAR(255)
 );
@@ -72,30 +74,24 @@ CREATE TABLE Transaction (
     FOREIGN KEY (Product_Id) REFERENCES Product(Product_Id)
 );
 
--- 4. Se desactiva la actualizacion
+-- 4. Se desactiva la actualización segura
 SET SQL_SAFE_UPDATES = 0;
 
 -- 5. Cargar datos desde el CSV
-
--- Si se usa MySQL la ruta es: 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/new_retail_data.csv' :
--- Si se usa phpMyAdmin la ruta es: 'C:/xampp/mysql/data/new_retail_data.csv'
--- Si se usa DBeaver la ruta es: 'C:/Users/tu_usuario/Documents/new_retail_data.csv'
--- Si se usa phpMyAdmin la ruta es: 'C:/xampp/mysql/data/new_retail_data.csv'
-
--- Se remplaza 'ruta/archivo/new_retail_data.csv' (Recueda colocar el archivo en la ruta)
-LOAD DATA INFILE 'ruta/archivo/new_retail_data.csv' 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/new_retail_data.csv'
 INTO TABLE store_inventory
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES
 (@Transaction_ID, @Customer_ID, @Name, @Email, @Phone, @Address, @City, @State, @Zipcode, @Country, @Age, @Gender, @Income, @Customer_Segment, @Date, @Year, @Month, @Time, @Total_Purchases, @Amount, @Total_Amount, @Product_Category, @Product_Brand, @Product_Type, @Feedback, @Shipping_Method, @Payment_Method, @Order_Status, @Ratings, @Products)
 SET 
+    City = NULLIF(@City, ''),
     State = NULLIF(@State, ''),
     Country = NULLIF(@Country, ''),
     Age = NULLIF(@Age, ''),
     Gender = NULLIF(@Gender, ''),
     Income = NULLIF(@Income, ''),
     Customer_Segment = NULLIF(@Customer_Segment, ''),
-    Date = IF(@Date REGEXP '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$', STR_TO_DATE(@Date, '%m/%d/%Y'), NULL), -- Validación segura de fecha
+    Date = IF(@Date REGEXP '^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$', STR_TO_DATE(@Date, '%m/%d/%Y'), NULL),
     Time = NULLIF(@Time, ''),
     Total_Purchases = NULLIF(@Total_Purchases, ''),
     Amount = NULLIF(@Amount, ''),
@@ -114,38 +110,42 @@ UPDATE store_inventory
 SET Products = 'Books'
 WHERE Areas = 'Reading';
 
--- 4. Actualizar valores de Subcategory según la clasificación
-UPDATE store_inventory SET Subcategory = 'Speculative Fiction' WHERE Subcategory IN ('Fantasy', 'Science Fiction', 'Dystopian');
-UPDATE store_inventory SET Subcategory = 'Realistic Fiction' WHERE Subcategory IN ('Literary Fiction', 'Historical Fiction');
-UPDATE store_inventory SET Subcategory = 'Genre Fiction' WHERE Subcategory IN ('Adventure', 'Horror', 'Mystery', 'Romance', 'Action');
-UPDATE store_inventory SET Subcategory = 'Life Writing' WHERE Subcategory IN ('Biography', 'Memoir');
-UPDATE store_inventory SET Subcategory = 'Professional & Academic' WHERE Subcategory IN ('Business', 'Psychology', 'Science');
-UPDATE store_inventory SET Subcategory = 'Practical & Lifestyle' WHERE Subcategory IN ('Cooking', 'Health', 'Self-help', 'Travel');
-UPDATE store_inventory SET Subcategory = 'Thriller' WHERE Subcategory IN ('Crime', 'Detective', 'Legal Thriller', 'Political Thriller', 'Techno-thriller', 'Psychological Thriller', 'Suspense');
-UPDATE store_inventory SET Subcategory = 'Classical/Canonical' WHERE Subcategory IN ('Classic Literature', 'Drama', 'Poetry');
-UPDATE store_inventory SET Subcategory = 'Modern/Contemporary' WHERE Subcategory IN ('Contemporary Literature', 'Modern Literature', 'Short Stories');
-UPDATE store_inventory SET Subcategory = 'Forms & Styles' WHERE Subcategory IN ('Anthologies', 'Essays');
-
 -- 7. Corregir nombres de Product_Brand
 UPDATE store_inventory SET Product_Brand = 'Mitsubishi' WHERE Product_Brand = 'Mitsubhisi';
 UPDATE store_inventory SET Product_Brand = 'Whirlpool' WHERE Product_Brand = 'Whirepool';
 
--- 8. Reemplazar Areas con Product_Type siempre que Areas es 'Home Decor'
+-- 8. Reemplazar valores en Products
+UPDATE store_inventory
+SET Products = 'air-conditioning'
+WHERE Products IN ('Mitsubishi 1.5 Ton 3 Star Split AC', 'BlueStar AC');
+
+-- 9. Reemplazar Areas con Product_Type siempre que Areas sea 'Home Decor'
 UPDATE store_inventory
 SET Areas = Product_Type
 WHERE Areas = 'Home Decor';
 
--- 9. Si Product_Type es 'Children\'s' y Products es 'Books', cambiar Areas a 'Children's'
+-- 10. Mover la primera palabra de Products a Product_Brand
+UPDATE store_inventory
+SET 
+    Product_Brand = SUBSTRING_INDEX(Products, ' ', 1), -- Extrae la primera palabra
+    Products = TRIM(SUBSTRING(Products FROM LOCATE(' ', Products) + 1)) -- Elimina la primera palabra
+WHERE Products REGEXP '^(Lenovo|Samsung|Huawei|Amazon|Google|Microsoft|Android|LG|Xiaomi|Razer|Asus|Nokia|Acer|Motorola|Sony)\\b';
+
+UPDATE store_inventory
+SET Product_Brand = 'Apple'
+WHERE Products IN ('iPad', 'iPhone');
+
+-- 11. Si Product_Type es 'Children\'s' y Products es 'Books', cambiar Areas a 'Children\'s'
 UPDATE store_inventory
 SET Areas = 'Children\'s'
 WHERE Product_Type = 'Children\'s' AND Products = 'Books';
 
--- 10. Si Areas es 'Children's', eliminar el valor de Subcategory
+-- 12. Si Areas es 'Children\'s', eliminar el valor de Subcategory
 UPDATE store_inventory
 SET Subcategory = NULL
 WHERE Areas = 'Children\'s';
 
--- 11. Mover Products a Subcategory en ciertas condiciones
+-- 13. Mover Products a Subcategory en ciertas condiciones
 UPDATE store_inventory
 SET Subcategory = Products
 WHERE Areas IN ('Clothing', 'Grocery');
@@ -156,7 +156,7 @@ WHERE Areas = 'Electronics'
 AND Product_Brand NOT IN ('Apple', 'Samsung', 'Sony') 
 AND Product_Brand IS NOT NULL;
 
--- 12. Asignar Product_Type a Products en ciertas condiciones
+-- 14. Asignar Product_Type a Products en ciertas condiciones
 UPDATE store_inventory
 SET Products = Product_Type
 WHERE Areas IN ('Clothing', 'Grocery');
@@ -167,11 +167,11 @@ WHERE Areas = 'Electronics'
 AND Product_Brand NOT IN ('Apple', 'Samsung', 'Sony') 
 AND Product_Brand IS NOT NULL;
 
--- 13. Se realizan las inserciones a cada tabla
+-- 15. Se realizan las inserciones a cada tabla
 
---  Insertar datos en Location (Evitar duplicados)
-INSERT INTO Location (State, Country)
-SELECT DISTINCT State, Country FROM store_inventory;
+-- Insertar datos en Location (Evitar duplicados)
+INSERT INTO Location (City, State, Country)
+SELECT DISTINCT City, State, Country FROM store_inventory;
 
 -- Insertar datos en Product (Evitar duplicados)
 INSERT INTO Product (Subcategory, Areas, Products, Brand)
@@ -180,9 +180,10 @@ SELECT DISTINCT Subcategory, Areas, Products, Product_Brand FROM store_inventory
 -- Insertar datos en Customer (Usar Location_Id como FK)
 INSERT INTO Customer (Location_Id, Age, Gender, Income, Segment)
 SELECT DISTINCT 
-    (SELECT Location_Id FROM Location WHERE store_inventory.State = Location.State AND store_inventory.Country = Location.Country),
+    (SELECT Location_Id FROM Location WHERE store_inventory.City = Location.City AND store_inventory.State = Location.State AND store_inventory.Country = Location.Country LIMIT 1),
     Age, Gender, Income, Customer_Segment 
 FROM store_inventory;
+
 
 -- Insertar datos en Transaction (Usar Customer_Id y Product_Id como FK)
 INSERT INTO Transaction (Customer_Id, Product_Id, Date, Time, Total, Amount, Feedback, Payment, Status, Ratings)
@@ -190,12 +191,11 @@ SELECT
     (SELECT Customer_Id FROM Customer WHERE store_inventory.Age = Customer.Age AND store_inventory.Gender = Customer.Gender AND store_inventory.Income = Customer.Income AND store_inventory.Customer_Segment = Customer.Segment LIMIT 1),
     (SELECT Product_Id FROM Product WHERE store_inventory.Products = Product.Products AND store_inventory.Product_Brand = Product.Brand AND store_inventory.Areas = Product.Areas LIMIT 1),
     Date, Time, Total_Purchases, Amount, Feedback, Payment_Method, Order_Status, Ratings 
-FROM store_inventory
-LIMIT 100000;
+FROM store_inventory;
 
--- 14. Se elimina store_inventory porque ya no se va a usar
+-- 16. Se elimina store_inventory porque ya no se va a usar
+ALTER TABLE Location DROP COLUMN City;
 DROP TABLE IF EXISTS store_inventory;
 
--- Se reactiva la actualizacion segura
+-- Se reactiva la actualización segura
 SET SQL_SAFE_UPDATES = 1;
-
